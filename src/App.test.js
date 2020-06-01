42493ffeeb560a8bbfb7a547c5565f33c82b8de9
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2020-06-01 15:10:56 +0200

    Update main page

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders coming soon', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Coming Soon!/i);
   expect(linkElement).toBeInTheDocument();
 });
