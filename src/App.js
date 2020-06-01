032341ca84715e90ee24a69a929c2cfc5fc04535
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2020-06-01 21:31:10 +0200

    abd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Coming Soon!</header>
+      <header className="App-header">aComing Soon!</header>
     </div>
   );
 }
