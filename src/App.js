9ce589fc46a55bf026bce5de8f8fc1d099e2c05b
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2020-06-03 23:03:46 +0200

    Add countdown and update css for it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Particles from 'react-particles-js';
+import Countdown from 'react-countdown';
 import './App.css'
 
 
 const particleConfig = require('./particlesjs-config.json')
+const finalDay = new Date('2020-06-10T12:00:00')
 
 function App() {
   return (
       <div className="App">
         <Particles className="particle-canvas" params={particleConfig}/>
+        <div className="countdown">
+          <Countdown date={finalDay} />
+        </div>
       </div>
   );
 }
